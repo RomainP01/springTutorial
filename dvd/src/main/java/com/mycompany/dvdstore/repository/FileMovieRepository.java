b0854ef7838e32d0ff594df83dc8334a12824472
Rhummboy <romainpanii@gmail.com>
2022-01-05 18:34:43 +0100

    exercice 10

--- dvd/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ dvd/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -4,6 +4,7 @@ import com.mycompany.dvdstore.entity.Movie;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -12,14 +13,15 @@ import java.io.IOException;
 @Data
 public class FileMovieRepository implements MovieRepositoryInterface {
     FileWriter fileWriter;
+    @Value("${movies.file.location}")
     File file;
-    public void add(Movie movie){
-        try{
-            fileWriter=new FileWriter(file,true);
-            fileWriter.write(movie.getTitle()+";"+movie.getGenre()+"\n");
+
+    public void add(Movie movie) {
+        try {
+            fileWriter = new FileWriter(file, true);
+            fileWriter.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
             fileWriter.close();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
