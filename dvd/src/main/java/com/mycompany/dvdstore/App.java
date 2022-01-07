5f9e0bbc68bbe706890b06fa9a30ed6698cef04f
Rhummboy <romainpanii@gmail.com>
2022-01-07 11:53:39 +0100

    censé fonctionner mais non

--- dvd/src/main/java/com/mycompany/dvdstore/App.java
+++ dvd/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,27 +1,16 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieControllerInterface;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan(basePackages = {
-        "com.mycompany.dvdstore.controller",
-        "com.mycompany.dvdstore.service",
-        "com.mycompany.dvdstore.repository.file"
-})
-@PropertySource("classpath:application.properties")
-//@ImportResource("classpath:applicationContext.xml")
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
         //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieControllerInterface movieController = context.getBean(MovieControllerInterface.class);
         movieController.addUsingConsole();
 
